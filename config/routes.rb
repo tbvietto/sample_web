137166afdec2ef70192807c1c5fa5860e0ef2589
Thai Viet <tbv.bkhn@gmail.com>
2018-03-14 01:50:26 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   root "static_pages#home"
 
+  get 'users/new'
   get "/about", to: "static_pages#about"
-
   get "/contact", to: "static_pages#contact"
-
   get "/help", to: "static_pages#help"
-
   get "/signup", to: "users#new"
-
+  post "/signup", to: "users#create"
+  resources :users
 end
